016f87a01487d57deac6210c8820d183e744a3cd
RogerXavier <rogerxavier10@gmail.com>
2023-05-24 22:21:33 -0300

    pipe operators

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Animal } from 'src/app/Animal';
 
 @Component({
   selector: 'app-list-render',
@@ -6,22 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./list-render.component.css'],
 })
 export class ListRenderComponent {
-  animals = [
+  animals: Animal[] = [
     {
       name: 'Belinha',
       type: 'dog',
+      age: 4,
     },
     {
       name: 'Kiara',
       type: 'Dinossaur',
+      age: 3,
     },
     {
       name: 'Globs',
       type: 'sloth',
+      age: 7,
     },
     {
       name: 'Bob',
       type: 'Horse',
+      age: 2,
     },
   ];
+
+  animalAge: string = '';
+
+  ShowAge(animal: Animal) {
+    this.animalAge = `The ${animal.name} is ${animal.age} years old`;
+  }
 }
